82a0ca92890e7e9aa82940fb9a65df8ee049122a
stokaboka <igothorev@gmail.com>
2019-08-06 12:16:12 +0300

    feat: add new technology; refactor VendorLogo

--- src/components/OrangemTechnology.vue
+++ src/components/OrangemTechnology.vue
@@ -7,7 +7,7 @@
     <div class="short-article__text short-article__text-font">
       We are developing information systems on <vendor-logo vendor="opensource"/> technologies.
       Applications are created using advanced web technologies:
-      <vendor-logo vendor="angularjs"/>, <vendor-logo vendor="angular"/> and now <vendor-logo vendor="vuejs"/>
+      <vendor-logo vendor="angularjs"/>, <vendor-logo vendor="angular"/> and now <vendor-logo vendor="vuejs"/>, <vendor-logo vendor="quasar"/>
       <p>
         We build front-end with <vendor-logo vendor="nodejs"/>, <vendor-logo vendor="webpack"/>,<vendor-logo vendor="postcss"/> and <vendor-logo vendor="babel"/>
       </p>
@@ -20,11 +20,15 @@
     </header>
     <div slot="text" class="short-article__text short-article__text-font">
       <p>
-        The heart and driving force behind our projects is <vendor-logo vendor="express"/> or <vendor-logo vendor="ias"/> running on <vendor-logo vendor="nodejs"/> and <vendor-logo vendor="ubuntu"/>
+        The heart and driving force behind our projects is <vendor-logo vendor="nestjs"/>,
+        <vendor-logo vendor="express"/>,
+        <vendor-logo vendor="ias"/>
+        running on <vendor-logo vendor="nodejs"/> under <vendor-logo vendor="docker"/> and <vendor-logo vendor="ubuntu"/>
       </p>
       <p>For data storage we use various systems:
         <vendor-logo vendor="mysql"/>, <vendor-logo vendor="mongodb"/>, <vendor-logo vendor="postgre"/>
       <p>We have experience with <span><a class="a-href-link__text" target="_blank" href="https://www.oracle.com/ru/database/index.html">Oracle Database</a></span> and <span><a class="a-href-link__text" target="_blank" href="https://www.microsoft.com/sql-server">MS-SQL Server</a></span>.</p>
+
     </div>
   </article>
 
--- src/components/VendorLogo.vue
+++ src/components/VendorLogo.vue
@@ -7,8 +7,36 @@
 </template>
 
 <script>
-
 import IconText from '@/components/text/IconText'
+
+const vendors = {
+  angularjs: { icon: require('../../static/assets/AngularJS-Shield.svg'), href: 'https://angularjs.org', title: 'AngularJS' },
+  angular: { icon: require('../../static/assets/angular.svg'), href: 'https://angular.io/', title: 'Angular' },
+  vuejs: { icon: require('../../static/assets/vuejs-seeklogo.com.svg'), href: 'https://vuejs.org', title: 'VueJS' },
+  nodejs: { icon: require('../../static/assets/nodejs.svg'), href: 'https://nodejs.org', title: 'Node.js' },
+  webpack: { icon: require('../../static/assets/webpack-small-logo.svg'), href: 'https://webpack.js.org/', title: 'Webpack' },
+  postcss: { icon: require('../../static/assets/postcss.svg'), href: 'http://postcss.org/', title: 'PostCSS' },
+  babel: { icon: require('../../static/assets/babel.svg'), href: 'https://babeljs.io/', title: 'Babel' },
+  ias: { icon: require('../../static/assets/ias.svg'), href: 'https://github.com/metarhia/impress', title: 'Impress Application Server (IAS)' },
+  ubuntu: { icon: require('../../static/assets/ubuntu.svg'), href: 'https://www.ubuntu.com/server', title: 'Linux/Ubuntu Server' },
+  mysql: { icon: require('../../static/assets/logo-mysql.svg'), href: 'https://www.mysql.com/', title: 'MySQL' },
+  mongodb: { icon: require('../../static/assets/MongoDB_Gray_Logo_FullColor_RGB.svg'), href: 'https://www.mongodb.com/', title: 'MongoDB' },
+  postgre: { icon: require('../../static/assets/PostgreSQL_logo.3colors.svg'), href: 'https://www.postgresql.org/', title: 'PostgreSQL' },
+  hadoop: { icon: require('../../static/assets/hadoop2.png'), href: 'http://hadoop.apache.org/', title: 'Apache Hadoop' },
+  spark: { icon: require('../../static/assets/spark.png'), href: 'https://spark.apache.org/', title: 'Apache Spark' },
+  hive: { icon: require('../../static/assets/hive.png'), href: 'https://hive.apache.org/', title: 'Apache Hive' },
+  pig: { icon: require('../../static/assets/pig.png'), href: 'https://pig.apache.org/', title: 'Apache Pig' },
+  livy: { icon: require('../../static/assets/livy.png'), href: 'https://livy.incubator.apache.org/', title: 'Apache Livy' },
+  opensource: { icon: require('../../static/assets/open-source.svg'), href: 'https://opensource.org/', title: 'Open Source' },
+  ige: { icon: require('../../static/assets/irrelon.png'), href: 'https://www.isogenicengine.com/', title: 'Isogenic Game Engine' },
+  express: { icon: require('../../static/assets/express.png'), href: 'https://expressjs.com/', title: 'Express' },
+  nestjs: { icon: require('../../static/assets/nestjs.svg'), href: 'https://nestjs.com/', title: 'NestJS' },
+  docker: { icon: require('../../static/assets/docker.png'), href: 'https://www.docker.com/', title: 'Docker' },
+  quasar: { icon: require('../../static/assets/quasar-logo.svg'), href: 'https://quasar.dev/', title: 'Quasar Framework' }
+}
+
+const vendorsKeys = Object.keys(vendors)
+
 export default {
   name: 'VendorLogo',
   components: {IconText},
@@ -17,8 +45,7 @@ export default {
       type: String,
       required: true,
       validator: function (value) {
-        const vendors = ['angularjs', 'angular', 'vuejs', 'nodejs', 'webpack', 'postcss', 'babel', 'ias', 'ubuntu', 'mysql', 'mongodb', 'postgre', 'hadoop', 'spark', 'hive', 'pig', 'livy', 'opensource', 'ige', 'express']
-        if (vendors.includes(value)) {
+        if (vendorsKeys.includes(value)) {
           return true
         } else {
           console.warn('Unknown vendor: ' + value)
@@ -29,40 +56,16 @@ export default {
   },
   data () {
     return {
-      vendors: {
-        angularjs: { icon: require('../../static/assets/AngularJS-Shield.svg'), href: 'https://angularjs.org', title: 'AngularJS' },
-        angular: {icon: require('../../static/assets/angular.svg'), href: 'https://angular.io/', title: 'Angular'},
-        vuejs: { icon: require('../../static/assets/vuejs-seeklogo.com.svg'), href: 'https://vuejs.org', title: 'VueJS' },
-        nodejs: { icon: require('../../static/assets/nodejs.svg'), href: 'https://nodejs.org', title: 'Node.js' },
-        webpack: { icon: require('../../static/assets/webpack-small-logo.svg'), href: 'https://webpack.js.org/', title: 'Webpack' },
-        postcss: { icon: require('../../static/assets/postcss.svg'), href: 'http://postcss.org/', title: 'PostCSS' },
-        babel: { icon: require('../../static/assets/babel.svg'), href: 'https://babeljs.io/', title: 'Babel' },
-        ias: { icon: require('../../static/assets/ias.svg'), href: 'https://github.com/metarhia/impress', title: 'Impress Application Server (IAS)' },
-        ubuntu: { icon: require('../../static/assets/ubuntu.svg'), href: 'https://www.ubuntu.com/server', title: 'Linux/Ubuntu Server' },
-        mysql: { icon: require('../../static/assets/logo-mysql.svg'), href: 'https://www.mysql.com/', title: 'MySQL' },
-        mongodb: { icon: require('../../static/assets/MongoDB_Gray_Logo_FullColor_RGB.svg'), href: 'https://www.mongodb.com/', title: 'MongoDB' },
-        postgre: { icon: require('../../static/assets/PostgreSQL_logo.3colors.svg'), href: 'https://www.postgresql.org/', title: 'PostgreSQL' },
-        hadoop: { icon: require('../../static/assets/hadoop2.png'), href: 'http://hadoop.apache.org/', title: 'Apache Hadoop' },
-        spark: { icon: require('../../static/assets/spark.png'), href: 'https://spark.apache.org/', title: 'Apache Spark' },
-        hive: { icon: require('../../static/assets/hive.png'), href: 'https://hive.apache.org/', title: 'Apache Hive' },
-        pig: { icon: require('../../static/assets/pig.png'), href: 'https://pig.apache.org/', title: 'Apache Pig' },
-        livy: { icon: require('../../static/assets/livy.png'), href: 'https://livy.incubator.apache.org/', title: 'Apache Livy' },
-        opensource: { icon: require('../../static/assets/open-source.svg'), href: 'https://opensource.org/', title: 'Open Source' },
-        ige: { icon: require('../../static/assets/irrelon.png'), href: 'https://www.isogenicengine.com/', title: 'Isogenic Game Engine' },
-        express: { icon: require('../../static/assets/express.png'), href: 'https://expressjs.com/', title: 'Express' }
-      },
-
+      vendors,
       unknownVendor: { icon: null, href: '', title: this.vendor }
     }
   },
 
   computed: {
     vendorInfo () {
-      if (this.vendors.hasOwnProperty(this.vendor)) {
-        return this.vendors[this.vendor]
-      } else {
-        return this.unknownVendor
-      }
+      return this.vendors.hasOwnProperty(this.vendor)
+        ? this.vendors[this.vendor]
+        : this.unknownVendor
     }
   }
 }
